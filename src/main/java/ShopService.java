0b02cfb3689b52d32fd6caa90921219f80ae4c3b
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-09-05 11:55:39 +0200

    Add ShopService

---
+++ src/main/java/ShopService.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ShopService {
+    private ProductRepo productRepo = new ProductRepo();
+    private OrderListRepo orderListRepo = new OrderListRepo();
+
+    public Order addOrder(List<String> productIds) {
+        List<Product> products = new ArrayList<>();
+
+        for (String productId : productIds) {
+            Product productToOrder = productRepo.getProductById(productId);
+            products.add(productToOrder);
+        }
+
+        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+
+        return orderListRepo.addOrder(newOrder);
+    }
+}
---
+++ src/test/java/ShopServiceTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopServiceTest {
+
+    @Test
+    void addOrderTest() {
+        //GIVEN
+        ShopService shopService = new ShopService();
+        List<String> productsIds = List.of("1");
+
+        //WHEN
+        Order actual = shopService.addOrder(productsIds);
+
+        //THEN
+        Order expected = new Order("-1", List.of(new Product("1", "Apfel")));
+        assertEquals(expected.products(), actual.products());
+        assertNotNull(expected.id());
+    }
+}
